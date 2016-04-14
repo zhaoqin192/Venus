704ad618b7ab55281f923319971762f2932d59a3
zhaoqin <zhaoqin@neotel.com.cn>
2016-04-14 17:37:18 +0800

    create project

---
+++ Venus/PresentationLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Venus
+//
+//  Created by zhaoqin on 4/14/16.
+//  Copyright © 2016 Neotel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Venus/PresentationLayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Venus
+//
+//  Created by zhaoqin on 4/14/16.
+//  Copyright © 2016 Neotel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
