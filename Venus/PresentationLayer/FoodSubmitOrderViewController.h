447d592a03b0fc6248d912e6ac5f4b88fbb5879c
EdwinZhou <zhouzhi@neotel.com.cn>
2016-05-17 21:55:25 +0800

    增加部分支付页面

---
+++ Venus/PresentationLayer/FoodSubmitOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodSubmitOrderViewController.h
+//  Venus
+//
+//  Created by EdwinZhou on 16/5/17.
+//  Copyright © 2016年 Neotel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodSubmitOrderViewController : UIViewController
+
+@end
