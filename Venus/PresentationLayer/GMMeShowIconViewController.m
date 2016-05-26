465cb53814b152392723f9ea369ebdfed1703d1c
wangxiao <wangxiao@ebupt.com>
2016-05-26 14:54:38 +0800

    实现保存头像到本地

--- Venus/PresentationLayer/GMMeShowIconViewController.m
+++ Venus/PresentationLayer/GMMeShowIconViewController.m
@@ -21,6 +21,27 @@
     if (self.imgUrl) {
         [self.imageView sd_setImageWithURL:[NSURL URLWithString:self.imgUrl]];
     }
+    self.navigationItem.rightBarButtonItem = ({
+        UIBarButtonItem *right = [[UIBarButtonItem alloc] initWithTitle:@"保存" style:UIBarButtonItemStyleDone target:self action:@selector(rightItemTapped)];
+        right;
+    });
+}
+
+- (void)rightItemTapped {
+    if (self.imageView) {
+        UIImageWriteToSavedPhotosAlbum(self.imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    }
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *) error contextInfo:(void *) contextInfo {
+    if (error != NULL) {
+        [SVProgressHUD showErrorWithStatus:@"保存图片失败"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:1.5];
+    }
+    else {
+        [SVProgressHUD showSuccessWithStatus:@"保存图片成功"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:1.5];
+    }
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -31,7 +52,11 @@
 - (void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
     [self.rdv_tabBarController setTabBarHidden:NO];
-} 
+}
+
+- (void)dismiss {
+    [SVProgressHUD dismiss];
+}
 
 /*
 #pragma mark - Navigation
